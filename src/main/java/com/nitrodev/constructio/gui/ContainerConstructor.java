c9bbc3359e1347aafb45ccb58ab8b48f497489b4
Nitrodev <jukka.keranen@hotmail.fi>
2016-01-05 23:55:56 +0200

    This will never stop...

--- src/main/java/com/nitrodev/constructio/gui/ContainerConstructor.java
+++ src/main/java/com/nitrodev/constructio/gui/ContainerConstructor.java
@@ -8,6 +8,8 @@ import net.minecraft.item.ItemStack;
 
 public class ContainerConstructor extends Container {
     private IInventory constructorInventory;
+    static final int MachineSlots = 9;
+    static final int PlayerSlots = 9*4;
 
     public ContainerConstructor(IInventory tileEntity, IInventory playerInv) {
         this.constructorInventory = playerInv;
@@ -36,33 +38,56 @@ public class ContainerConstructor extends Container {
         return this.constructorInventory.isUseableByPlayer(player);
     }
 
-    public ItemStack transferStackInSlot(EntityPlayer playerEntity, int i) {
-        ItemStack lvt_3_1_ = null;
-        Slot lvt_4_1_ = (Slot)this.inventorySlots.get(i);
-        if(lvt_4_1_ != null && lvt_4_1_.getHasStack()) {
-            ItemStack lvt_5_1_ = lvt_4_1_.getStack();
-            lvt_3_1_ = lvt_5_1_.copy();
-            if(i < 9) {
-                if(!this.mergeItemStack(lvt_5_1_, 9, 45, true)) {
-                    return null;
-                }
-            } else if(!this.mergeItemStack(lvt_5_1_, 0, 9, false)) {
-                return null;
-            }
+    //For shift-clicking from the inventory
+    //If return null then can't be shift-clicked
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer playerEntity, int slotIndex) {
 
-            if(lvt_5_1_.stackSize == 0) {
-                lvt_4_1_.putStack((ItemStack)null);
-            } else {
-                lvt_4_1_.onSlotChanged();
-            }
+        Slot slot = this.inventorySlots.get(slotIndex);
+        if (slot == null || !slot.getHasStack()) {
 
-            if(lvt_5_1_.stackSize == lvt_3_1_.stackSize) {
-                return null;
-            }
-
-            lvt_4_1_.onPickupFromSlot(playerEntity, lvt_5_1_);
+            return null;
         }
 
-        return lvt_3_1_;
+        ItemStack stack = slot.getStack();
+        ItemStack stackCopy = stack.copy();
+
+        int startIndex;
+        int endIndex;
+
+        if (slotIndex < MachineSlots) {
+
+            startIndex = MachineSlots;
+            endIndex = MachineSlots + PlayerSlots;
+
+        } else {
+
+            startIndex = 0;
+            endIndex = MachineSlots;
+        }
+
+
+
+        if (!this.mergeItemStack(stack, startIndex, endIndex, false))
+        {
+            return null;
+        }
+
+        if (stack.stackSize == 0)
+        {
+            slot.putStack(null);
+        }
+        else
+        {
+            slot.onSlotChanged();
+        }
+
+        if (stack.stackSize == stackCopy.stackSize)
+        {
+            return null;
+        }
+
+        slot.onPickupFromSlot(playerEntity, stack);
+        return stackCopy;
     }
 }
