0cc343a7ee402a9ca89248989bc01c0351e5431b
Nitrodev <jukka.keranen@hotmail.fi>
2016-01-11 15:55:57 +0200

    I'm running out of names

--- src/main/java/com/nitrodev/constructio/recipes/CioShapedRecipe.java
+++ src/main/java/com/nitrodev/constructio/recipes/CioShapedRecipe.java
@@ -2,8 +2,8 @@ package com.nitrodev.constructio.recipes;
 
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
 public class CioShapedRecipe extends ShapedRecipes {
@@ -12,6 +12,7 @@ public class CioShapedRecipe extends ShapedRecipes {
     public final int recipeHeight;
     public final ItemStack[] recipeItems;
     public final ItemStack recipeOutput;
+    private boolean copyIngredientNBT;
 
     public CioShapedRecipe(int width, int height, ItemStack[] recipeItems, ItemStack output) {
         super(width, height, recipeItems, output);
@@ -23,18 +24,80 @@ public class CioShapedRecipe extends ShapedRecipes {
     }
 
     @Override
-    public ItemStack getCraftingResult(InventoryCrafting inv) {
-        return super.getCraftingResult(inv);
+    public ItemStack[] getRemainingItems(InventoryCrafting inv) {
+
+        ItemStack[] aitemstack = new ItemStack[inv.getSizeInventory()];
+
+        for (int i = 0; i < aitemstack.length; ++i) {
+            ItemStack itemstack = inv.getStackInSlot(i);
+            aitemstack[i] = net.minecraftforge.common.ForgeHooks.getContainerItem(itemstack);
+        }
+        return aitemstack;
     }
 
     @Override
     public boolean matches(InventoryCrafting inv, World worldIn) {
-        return super.matches(inv, worldIn);
+        for (int i = 0; i <= 5 - this.recipeWidth; ++i) {
+            for (int j = 0; j <= 5 - this.recipeHeight; ++j) {
+                if (this.checkMatch(inv, i, j, true)) {
+                    return true;
+                }
+                if (this.checkMatch(inv, i, j, false)){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private boolean checkMatch(InventoryCrafting inv, int i, int j, boolean choice) {
+        for (int i1 = 0; i1 < 5; ++i1) {
+            for (int j1 = 0; j1 < 5; ++j1) {
+
+                int k = i1 - i;
+                int l = j1 - j;
+                ItemStack itemstack = null;
+
+                if (k >= 0 && l >= 0 && k < this.recipeWidth && l < this.recipeHeight) {
+                    if (choice) {
+                        itemstack = this.recipeItems[this.recipeWidth - k - 1 + l * this.recipeWidth];
+                    }
+                    else {
+                        itemstack = this.recipeItems[k + l * this.recipeWidth];
+                    }
+                }
+
+                ItemStack itemStack = inv.getStackInRowAndColumn(i1, j1);
+
+                if (itemStack != null || itemstack != null) {
+                    if (itemStack == null && itemstack != null || itemStack != null && itemstack == null) {
+                        return false;
+                    }
+                    if (itemstack.getItem() != itemStack.getItem()) {
+                        return false;
+                    }
+                    if (itemstack.getMetadata() != 32767 && itemstack.getMetadata() != itemStack.getMetadata()) {
+                        return false;
+                    }
+                }
+            }
+        }
+       return true;
     }
 
     @Override
-    public ItemStack[] getRemainingItems(InventoryCrafting inv) {
-        return super.getRemainingItems(inv);
+    public ItemStack getCraftingResult(InventoryCrafting inv) {
+        ItemStack itemStack = this.getRecipeOutput().copy();
+        if (this.copyIngredientNBT) {
+            for (int i = 0; i < inv.getSizeInventory(); ++i) {
+                ItemStack itemStack1 = inv.getStackInSlot(i);
+                if (itemStack1 != null && itemStack1.hasTagCompound()){
+                    itemStack.setTagCompound((NBTTagCompound)itemStack1.getTagCompound().copy());
+                }
+            }
+        }
+        return itemStack;
     }
 
     @Override
@@ -44,6 +107,6 @@ public class CioShapedRecipe extends ShapedRecipes {
 
     @Override
     public ItemStack getRecipeOutput() {
-        return super.getRecipeOutput();
+        return this.recipeOutput;
     }
 }
--- src/main/java/com/nitrodev/constructio/recipes/CiocraftManager.java
+++ src/main/java/com/nitrodev/constructio/recipes/CiocraftManager.java
@@ -1,13 +1,16 @@
 package com.nitrodev.constructio.recipes;
 
 import com.google.common.collect.Lists;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
 
 import java.util.List;
+import java.util.Map;
 
 public class CiocraftManager {
 
     private static final CiocraftManager instance = new CiocraftManager();
-    private final List recipes = Lists.newArrayList();
+    private final List<IRecipe> recipes = Lists.<IRecipe>newArrayList();
 
 
     public static CiocraftManager getInstance() {
@@ -17,6 +20,36 @@ public class CiocraftManager {
 
     private CiocraftManager() {
 
+    }
+
+    public CioShapedRecipe addRecipe(ItemStack itemStack, Object... recipeComponents) {
+        String s = "";
+        int i = 0;
+        int width = 0;
+        int height = 0;
+
+        if (recipeComponents[i] instanceof String[]) {
+            String[] astring = (String[])((String[])recipeComponents[i++]);
+
+            for (int l = 0; l < astring.length; ++l){
+                String s1 = astring[l];
+                ++height;
+                width = s1.length();
+                s = s + s1;
+            }
+        } else {
+            while (recipeComponents[i] instanceof String) {
+                String s2 = (String)recipeComponents[i++];
+                ++height;
+                width = s2.length();
+                s = s + s2;
+            }
+        }
+
+        Map<Character, ItemStack>map;
+
+
+        return null;
     }
 
 }
