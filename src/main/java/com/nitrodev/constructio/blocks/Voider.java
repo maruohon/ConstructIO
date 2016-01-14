71091ed9a3a6164bccfa476536d81edb4a88ee62
Nitrodev <jukka.keranen@hotmail.fi>
2016-01-14 18:12:55 +0200

    Retexturing

--- src/main/java/com/nitrodev/constructio/blocks/Voider.java
+++ src/main/java/com/nitrodev/constructio/blocks/Voider.java
@@ -11,6 +11,7 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.World;
 
 public class Voider extends Block {
@@ -22,6 +23,16 @@ public class Voider extends Block {
 
         this.setUnlocalizedName("blockVoider");
         this.setCreativeTab(ConstructIO.tabconstructio);
+    }
+
+    @Override
+    public EnumWorldBlockLayer getBlockLayer() {
+        return EnumWorldBlockLayer.TRANSLUCENT;
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
     }
 
     public AxisAlignedBB getCollisionBoundingBox(World p_getCollisionBoundingBox_1_, BlockPos p_getCollisionBoundingBox_2_, IBlockState p_getCollisionBoundingBox_3_) {
